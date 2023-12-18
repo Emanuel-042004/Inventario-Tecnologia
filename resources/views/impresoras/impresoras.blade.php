dd87c80ae3e8f543fae00ddd611349685ce98abc
Emanuel-042004 <charagomezemanuel@gmail.com>
2023-12-18 17:02:40 -0500

    Prueba para boton descargar

--- resources/views/impresoras/impresoras.blade.php
+++ resources/views/impresoras/impresoras.blade.php
@@ -15,7 +15,7 @@
             <h1 class="text-black "><strong>Impresoras </strong><img width="48" height="48"
                     src="https://img.icons8.com/emoji/48/printer.png" alt="printer">
                     @can('exportar')
-                <a href="{{ route('export.impresoras') }}" class="btn btn-light  mb-3 shadow"><svg xmlns="http://www.w3.org/2000/svg" width="20"
+                       <a href="{{ route('export.impresoras') }}" class="btn btn-light  mb-3 shadow"><svg xmlns="http://www.w3.org/2000/svg" width="20"
                         height="20" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
                         <path
                             d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5" />
--- resources/views/telefonos/celulares.blade.php
+++ resources/views/telefonos/celulares.blade.php
@@ -13,8 +13,8 @@
         </form>
       <h1 class="text-black "><strong>Celulares</strong>
         <img width="48" height="48" src="https://img.icons8.com/fluency/48/000000/smartphone.png" alt="smartphone" />
-        @can('exportar')
-                <a href="{{ route('export.celulares') }}" class="btn btn-light  mb-3 shadow"><svg xmlns="http://www.w3.org/2000/svg" width="20"
+              @can('exportar')
+                   <a href="{{ route('export.celulares') }}" class="btn btn-light  mb-3 shadow"><svg xmlns="http://www.w3.org/2000/svg" width="20"
                         height="20" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
                         <path
                             d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5" />
